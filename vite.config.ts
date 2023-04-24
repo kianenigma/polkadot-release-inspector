65d4985a2efab3c4b2ec3720fa534f2e6a5e9ec9
wirednkod <wirednkod@gmail.com>
2023-04-24 19:19:49 +0300

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     viteTsconfigPaths(),
@@ -18,5 +19,8 @@ export default defineConfig({
   ],
   server: {
     port: 3006,
+  },
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts']
   },
 });
